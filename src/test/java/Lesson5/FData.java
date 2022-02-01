09529d79e3d7a61375e422db791eb90115b5daa6
MarthaKoltinova <96679318+MarthaKoltinova@users.noreply.github.com>
2022-02-01 16:39:36 +0300

    Add files via upload

---
+++ src/test/java/Lesson5/FData.java
@@ -0,0 +1,25 @@
+package Lesson5;
+
+public class FData {
+    public String roomWidth;
+    public String roomHeight;
+    public String lamWidth;
+    public String lamHeight;
+    public String inpack;
+    public String pr;
+    public String direction;
+    public String offset;
+    public String retreat;
+
+    public FData(String roomWidth, String roomHeight, String lamWidth, String lamHeight, String inpack, String pr, String direction, String offset, String retreat) {
+        this.roomWidth = roomWidth;
+        this.roomHeight = roomHeight;
+        this.lamWidth = lamWidth;
+        this.lamHeight = lamHeight;
+        this.inpack = inpack;
+        this.pr = pr;
+        this.direction = direction;
+        this.offset = offset;
+        this.retreat = retreat;
+    }
+}
